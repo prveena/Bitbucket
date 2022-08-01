7be7176fd8ecc80486b1a58b0ab25401729cc220
prveena <praveena.vs97@gmail.com>
2022-08-01 19:09:51 +0530

    Flutter task 1 commit

---
+++ form/lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+
+class Contstants{
+  static String baseUrl='http://192.168.29.243:8000/';
+}
---
+++ form/lib/data/apiClient/api_client.dart
@@ -0,0 +1,5 @@
+import 'package:prveena_s_application1/core/app_export.dart';
+
+class ApiClient extends GetConnect {
+  
+}
